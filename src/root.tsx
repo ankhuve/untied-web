03d0ba432d2a4a28e053c1cc65188bf66728cae9
Johannes Mattsson <johannes@untied.world>
2023-04-20 17:29:21 +0200

    Add partytown and google tag manager
    https://qwik.builder.io/docs/integrations/partytown/

--- src/root.tsx
+++ src/root.tsx
@@ -5,6 +5,7 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/RouterHead/RouterHead";
+import { QwikPartytown } from "./components/partytown/partytown";
 
 import globalStyles from "./global.css?inline";
 
@@ -22,6 +23,12 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <link rel="manifest" href="/manifest.json" />
+        <QwikPartytown forward={['dataLayer.push']} />
+        <script
+          async
+          type="text/partytown"
+          src="https://www.googletagmanager.com/gtag/js?id=G-NCGTE4LP79"
+        />
         <RouterHead />
       </head>
       <body lang="en">
